db0a4ea20fc87389e7e92cd1b93b5aca1446dce7
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2020-08-04 10:39:09 +0700

    git update 4/8/2020

--- QuanLyBanHang/Common/Hash.cs
+++ QuanLyBanHang/Common/Hash.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Data.Models.Login;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace QuanLyBanHang.Common
 {
     public class Hash
     {
+        private readonly IConfiguration _config;
+
+        public Hash(IConfiguration config)
+        {
+            _config = config;
+        }
         public string HashPassword(string password)
         {
             var bytes = new UTF8Encoding().GetBytes(password);
@@ -19,6 +29,24 @@ namespace QuanLyBanHang.Common
                 hashBytes = algorithm.ComputeHash(bytes);
             }
             return Convert.ToBase64String(hashBytes);
+        }
+
+        public Hash()
+        {
+            
+        }
+        public string GenerateJSONWebToken(LoginModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- QuanLyBanHang/Startup.cs
+++ QuanLyBanHang/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Application.Data.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using QuanLyBanHang.Controllers;
 using QuanLyBanHang.Interface;
 using QuanLyBanHang.Repositories;
@@ -36,6 +39,20 @@ namespace QuanLyBanHang
             services.AddTransient<ILogin, LoginRepository>();
             services.AddTransient<INhanVien, EmployeeRepository>();
             services.AddTransient<IQueQuan, QueQuanRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>    
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +69,7 @@ namespace QuanLyBanHang
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
